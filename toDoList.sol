2098779fdb0beeac10d6a4ef034e389fa115ca52
Nawed Ali <nawed2611@gmail.com>
2022-07-24 22:25:40 +0530

    add hardhat formatting

--- toDoList.sol
+++ toDoList.sol
@@ -2,33 +2,25 @@
 
 pragma solidity >=0.7.0 <0.9.0;
 
-
-contract toDoList{
-    mapping (address => string[]) public todos;
+contract toDoList {
+    mapping(address => string[]) public todos;
 
     function addTodo(string memory _todo) public {
-
         todos[msg.sender].push(_todo);
+    }
 
-    } 
-
-    function getTodos() view public returns(string[] memory) {
-
+    function getTodos() public view returns (string[] memory) {
         return todos[msg.sender];
-    } 
+    }
 
-    function getNumberOfTodos() view public returns(uint){
-
+    function getNumberOfTodos() public view returns (uint) {
         return todos[msg.sender].length;
     }
-    
+
     function deleteTodo(uint _index) public {
+        require(_index <= getNumberOfTodos(), "Index doesn't exist!");
 
-        require(_index <= getNumberOfTodos() , "Index doesn't exist!");
-
-        todos[msg.sender][_index] = todos[msg.sender][getNumberOfTodos() - 1 ];
+        todos[msg.sender][_index] = todos[msg.sender][getNumberOfTodos() - 1];
         todos[msg.sender].pop();
-
-    } 
-
+    }
 }
